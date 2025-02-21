f3da434adfef46a42364eee29c48e856769cee50
Shert <shert.schauro@gmail.com>
2025-02-21 18:55:22 +0300

    [+] add fix imports [-] edit clang-format

--- kmumkit/file.cpp
+++ kmumkit/file.cpp
@@ -37,6 +37,43 @@ void fMapSections (IN void* pFile, OUT void* pImage) {
 	}
 }
 
+void fFixImports (IN void* pFile, OUT void* pImage) {
+	PIMAGE_NT_HEADERS64 pNtHeaders	   = IMAGE_NT_HEADERS64(pFile);
+	uintptr_t			importTableRVA = pNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
+	void*				importTableVA  = (void*)((uintptr_t)pImage + importTableRVA);
+	dbg("Import Table RVA = 0x%llX; Import Table VA = 0x%p", importTableRVA, importTableVA);
+
+	PIMAGE_IMPORT_DESCRIPTOR pDescriptor = (PIMAGE_IMPORT_DESCRIPTOR)importTableVA;
+	for (; *(uintptr_t*)pDescriptor; pDescriptor++) {
+		char*	szNameLib = (char*)((uintptr_t)pImage + pDescriptor->Name);
+		HMODULE hLib	  = LoadLibraryA(szNameLib);
+		chandle(hLib);
+
+		void* pIAT = (void*)((uintptr_t)pImage + pDescriptor->FirstThunk);
+		void* pOTs = (void*)((uintptr_t)pImage + pDescriptor->OriginalFirstThunk);
+		dbg("Start fix import for %s; pIAT = 0x%p; pOTs = 0x%p", szNameLib, pIAT, pOTs);
+
+		PIMAGE_THUNK_DATA64 pThunk	   = (PIMAGE_THUNK_DATA64)pOTs;
+		void**				ppFunction = (void**)pIAT;
+
+		for (; *(uintptr_t*)pThunk; pThunk++, ppFunction++) {
+			if (IMAGE_SNAP_BY_ORDINAL(*(uintptr_t*)pThunk)) {
+				void* pFunc = GetProcAddress(hLib, (char*)IMAGE_ORDINAL(pThunk->u1.Ordinal));
+				cnull(pFunc);
+				*ppFunction = pFunc;
+				dbg("Fix import by ordinal %llX", IMAGE_ORDINAL(pThunk->u1.Ordinal));
+				continue;
+			}
+
+			PIMAGE_IMPORT_BY_NAME pInfoImportByName = (PIMAGE_IMPORT_BY_NAME)((uintptr_t)pImage + pThunk->u1.AddressOfData);
+			void*				  pFunc				= GetProcAddress(hLib, pInfoImportByName->Name);
+			cnull(pFunc);
+			*ppFunction = pFunc;
+			dbg("Fix import by name %s", pInfoImportByName->Name);
+		}
+	}
+}
+
 void* fExecuteMap (IN const wchar_t* szPath) {
 	void* pFile = fFileMap(szPath);
 
@@ -45,6 +82,7 @@ void* fExecuteMap (IN const wchar_t* szPath) {
 	cnull(pImage);
 
 	fMapSections(pFile, pImage);
+	fFixImports(pFile, pImage);
 
 	czero(VirtualFree(pFile, 0, MEM_RELEASE));
 	return pImage;
